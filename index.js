4630fff4ed6506fe0d2620df31364d20980741e6
Arnold Paye <arnoldpayet@gmail.com>
2024-03-20 09:16:31 -0400

    navbar

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const h1 = document.createElement("h1");
-h1.textContent = "Hello world";
-h1.className = "header";
-console.log(h1);
-
-// <h1 class="header">Hello world</h1>
-
-const element = <h1 className="header">This is JSX</h1>;
-console.log(element);
-
-/*
-{
-    "type": "h1",
-    "key": null,
-    "ref": null,
-    "props": {
-        "className": "header",
-        "children": "This is JSX"
-    },
-    "_owner": null,
-    "_store": {}
-}
- */
-
 // JSX
-ReactDOM.render(element, document.getElementById("root"));
+const navbar = (
+  <nav>
+    <h1>Arnex</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>Abount</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+console.log(navbar);
+
+ReactDOM.render(navbar, document.getElementById("root"));
