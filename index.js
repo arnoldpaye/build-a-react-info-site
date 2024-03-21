7338ddfd18ca021896e692c92555b861a1f230af
Arnold Paye <arnoldpayet@gmail.com>
2024-03-21 09:15:06 -0400

    project part1 - markup

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// JSX
-const navbar = (
-  <nav>
-    <h1>Arnex</h1>
+const page = (
+  <div>
+    <img src="./react-logo.png" width="40px" alt="React Logo" />
+    <h1>Fun facts about React</h1>
     <ul>
-      <li>Pricing</li>
-      <li>Abount</li>
-      <li>Contact</li>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-  </nav>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+root.render(page);
